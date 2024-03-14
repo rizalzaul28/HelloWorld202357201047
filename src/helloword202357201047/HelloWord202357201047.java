7f3568d298e316939ed9f999232f721e13b47e29
rizalzaul28 <rizandi778@gmail.com>
2024-03-14 23:24:34 +0700

    first commit

---
+++ src/helloword202357201047/HelloWord202357201047.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package helloword202357201047;
+
+/**
+ *
+ * @author hp311
+ */
+public class HelloWord202357201047 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello Word");
+        // TODO code application logic here
+    }
+    
+}
